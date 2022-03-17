0fe7cfcb6769478dd342076e8d96cc1c6242c80d
arceda <enriquefirst@gmail.com>
2022-03-16 21:29:00 -0500

    training complete

--- APPM/script/train_keras.py
+++ APPM/script/train_keras.py
@@ -33,23 +33,46 @@ Y_test_labels = data_process.binary2onehot(data['Y_test'])
 X_train_data = data['X_train']  #already one hot encoded
 X_test_data = data['X_test']
 
+
+# simple model
 model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(11, 21, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(32, (2, 2), activation='relu', input_shape=(11, 21, 1)))
+model.add(layers.Conv2D(64, (2, 2), activation='relu'))
+#model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (2, 2), activation='relu'))
 
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+model.add(layers.Dense(2))
+
+
+'''
+model = models.Sequential()
+model.add(layers.Conv2D(128, (2, 2), strides=(1, 1), activation='relu', input_shape=(11, 21, 1)))
+model.add(layers.Conv2D(128, (2, 2), strides=(1, 1), activation='relu')) 
+model.add(layers.Conv2D(256, (2, 2), strides=(1, 1), activation='relu')) 
+model.add(layers.Conv2D(256, (2, 2), strides=(1, 1), activation='relu'))
+model.add(layers.Conv2D(256, (2, 2), strides=(1, 1), activation='relu'))
+model.add(layers.Conv2D(512, (1, 2), strides=(1, 1), activation='relu'))
+model.add(layers.Conv2D(512, (1, 1), strides=(1, 1), activation='relu'))
+model.add(layers.Conv2D(256, (1, 1), strides=(1, 1), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(2))'''
 
 model.summary()
 
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(X_train_data, Y_train_labels, epochs=10, 
-                    validation_data=(X_test_data, Y_test_labels))
+history = model.fit(X_train_data, Y_train_labels, epochs=20, validation_split = 0.1)
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'val'], loc='upper left')
+plt.show()
